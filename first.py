7afad74a19d53513b71e1130d5bc0582cc767a8d
Namanrai1222 <0241csds130@niet.co.in>
2025-12-30 22:41:43 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -1,6 +1,2 @@
-# # # myname=input()
-# # print("It is good to meet you, "+myname)
-# print(True and False)
-# print(False and True)
-# print(True or False)
-# print(False or True)
+myname=input()
+print("It is good to meet you, "+myname)
